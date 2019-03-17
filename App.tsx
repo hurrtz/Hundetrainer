8d1783c0cd03ccde20246f017a76f8ebf80824fb
Tobias Winkler <info@tobiaswinkler.berlin>
2019-03-17 20:08:37 +0100

    prettified files
    Signed-off-by: Tobias Winkler <info@tobiaswinkler.berlin>

--- App.tsx
+++ App.tsx
@@ -1,15 +1,5 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * 
- * Generated with the TypeScript template
- * https://github.com/emin93/react-native-template-typescript
- * 
- * @format
- */
-
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
