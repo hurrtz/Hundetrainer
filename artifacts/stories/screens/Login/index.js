cc0b53ab6e2af5676da4a8691e50d92f70124af9
Tobias Winkler <info@tobiaswinkler.berlin>
2019-03-18 23:38:38 +0100

    removed artifacts from git cache
    Signed-off-by: Tobias Winkler <info@tobiaswinkler.berlin>

--- artifacts/stories/screens/Login/index.js
+++ artifacts/stories/screens/Login/index.js
@@ -1,15 +1,21 @@
 import * as React from 'react';
-import { Image, Platform } from 'react-native';
-import { Container, Content, Header, Body, Title, Button, Text, View, Icon, Footer, } from 'native-base';
+import { Image } from 'react-native';
+import { Container, Content, Header, Button, Text, View, Footer, } from 'native-base';
+import GameImage from 'assets/images/bo/game.png';
+import PawImage from 'assets/images/paw.png';
 class Login extends React.Component {
     render() {
         return (React.createElement(Container, null,
-            React.createElement(Header, { style: { height: 200 } },
-                React.createElement(Body, { style: { alignItems: 'center' } },
-                    React.createElement(Icon, { name: "flash", style: { fontSize: 104 } }),
-                    React.createElement(Title, null, "ReactNativeSeed.com"),
-                    React.createElement(View, { padder: true },
-                        React.createElement(Text, { style: { color: Platform.OS === 'ios' ? '#000' : '#FFF' } })))),
+            React.createElement(Header, { transparent: true, noLeft: true, noShadow: true, style: {
+                    height: 200,
+                    padding: 10,
+                    paddingBottom: 0,
+                    backgroundColor: '#FFF',
+                    marginLeft: -10,
+                    marginTop: -15,
+                    marginRight: -10,
+                } },
+                React.createElement(Image, { source: GameImage, style: { width: '100%', height: '100%' } })),
             React.createElement(Content, null,
                 this.props.loginForm,
                 React.createElement(View, { padder: true },
@@ -19,7 +25,7 @@ class Login extends React.Component {
                 React.createElement(View, { style: { alignItems: 'center', opacity: 0.5, flexDirection: 'row' } },
                     React.createElement(View, { padder: true },
                         React.createElement(Text, { style: { color: '#000' } }, "May the dog be wuff you. ")),
-                    React.createElement(Image, { source: require('../../../assets/images/paw.png'), style: { width: 25, height: 25 } })))));
+                    React.createElement(Image, { source: PawImage, style: { width: 25, height: 25 } })))));
     }
 }
 export default Login;
