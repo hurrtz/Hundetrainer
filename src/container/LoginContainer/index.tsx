4a1830eff5cb1e845146babcb8ee530c77e31950
Tobias Winkler <info@tobiaswinkler.berlin>
2019-03-19 22:55:39 +0100

    temporarily disabled login check
    Signed-off-by: Tobias Winkler <info@tobiaswinkler.berlin>

--- src/container/LoginContainer/index.tsx
+++ src/container/LoginContainer/index.tsx
@@ -34,7 +34,7 @@ class LoginForm extends Component<Props, State> {
   login() {
     const { valid, navigation } = this.props;
 
-    if (valid) {
+    if (valid || true) {
       navigation.navigate('Drawer');
     } else {
       Toast.show({
