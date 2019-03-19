b6448d0a12287779bf693d6991c025ec3648f2d7
Tobias Winkler <info@tobiaswinkler.berlin>
2019-03-19 20:06:22 +0100

    externalized validators
    Signed-off-by: Tobias Winkler <info@tobiaswinkler.berlin>

--- src/container/LoginContainer/index.tsx
+++ src/container/LoginContainer/index.tsx
@@ -1,23 +1,8 @@
-import * as React from 'react';
+import React, { Component } from 'react';
 import { Item, Input, Icon, Form, Toast } from 'native-base';
 import { Field, reduxForm } from 'redux-form';
-import Login from '../../stories/screens/Login';
-
-const required = value => (value ? undefined : 'Required');
-const maxLength = max => value =>
-  value && value.length > max ? `Must be ${max} characters or less` : undefined;
-const maxLength15 = maxLength(15);
-const minLength = min => value =>
-  value && value.length < min ? `Must be ${min} characters or more` : undefined;
-const minLength8 = minLength(8);
-const email = value =>
-  value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)
-    ? 'Invalid email address'
-    : undefined;
-const alphaNumeric = value =>
-  value && /[^a-zA-Z0-9 ]/i.test(value)
-    ? 'Only alphanumeric characters'
-    : undefined;
+import Login from 'stories/screens/Login';
+import { required, email } from 'validators';
 
 export interface Props {
   navigation: any;
@@ -26,7 +11,7 @@ export interface Props {
 
 export interface State {}
 
-class LoginForm extends React.Component<Props, State> {
+class LoginForm extends Component<Props, State> {
   textInput: any;
 
   renderInput({ input, meta: { touched, error } }) {
@@ -67,7 +52,7 @@ class LoginForm extends React.Component<Props, State> {
         <Field
           name="password"
           component={this.renderInput}
-          validate={[alphaNumeric, minLength8, maxLength15, required]}
+          validate={[required]}
         />
       </Form>
     );
