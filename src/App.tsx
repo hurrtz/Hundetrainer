ee51505ff95d4489f3156614c19d5006ba0125fd
Tobias Winkler <info@tobiaswinkler.berlin>
2019-03-25 15:21:27 +0100

    renamed packages.json > package.json
    Signed-off-by: Tobias Winkler <info@tobiaswinkler.berlin>

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import { createAppContainer } from 'react-navigation';
 import { Root } from 'native-base';
 import { StatusBar } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './reducers';
 
 import DrawerNavigator from 'navigation/drawer';
 
@@ -11,13 +14,17 @@ interface Props {}
 
 interface State {}
 
+const store = createStore(rootReducer);
+
 class AppRoot extends Component<Props, State> {
   render() {
     return (
-      <Root>
-        <StatusBar hidden />
-        <App />
-      </Root>
+      <Provider store={store}>
+        <Root>
+          <StatusBar hidden />
+          <App />
+        </Root>
+      </Provider>
     );
   }
 }
