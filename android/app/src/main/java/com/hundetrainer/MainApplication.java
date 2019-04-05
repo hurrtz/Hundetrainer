ddbe25298458fbfa33ba1fcc2eb6367b7c74959f
Tobias Winkler <info@tobiaswinkler.berlin>
2019-04-05 15:45:47 +0200

    added shoutem component library
    Signed-off-by: Tobias Winkler <info@tobiaswinkler.berlin>

--- android/app/src/main/java/com/hundetrainer/MainApplication.java
+++ android/app/src/main/java/com/hundetrainer/MainApplication.java
@@ -3,6 +3,9 @@ package com.hundetrainer;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactnative.photoview.PhotoViewPackage;
+import cl.json.RNSharePackage;
+import com.BV.LinearGradient.LinearGradientPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -26,6 +29,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new PhotoViewPackage(),
+            new RNSharePackage(),
+            new LinearGradientPackage(),
             new AsyncStoragePackage(),
             new VectorIconsPackage(),
             new RNGestureHandlerPackage()
