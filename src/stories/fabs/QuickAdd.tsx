7cc29f85c13ed265e497d86576530904f01b4f7a
Tobias Winkler <info@tobiaswinkler.berlin>
2019-04-20 22:06:44 +0200

    re-implemented FAB
    Signed-off-by: Tobias Winkler <info@tobiaswinkler.berlin>

---
+++ src/stories/fabs/QuickAdd.tsx
@@ -0,0 +1,43 @@
+import React, { PureComponent } from 'react';
+import ActionButton from 'react-native-action-button';
+import { StyleSheet } from 'react-native';
+import IconComponent from 'react-native-vector-icons/MaterialCommunityIcons';
+
+import { TNavigation } from 'apptypes/base';
+
+interface Props {
+  navigation: TNavigation;
+}
+
+interface State {}
+
+const styles = StyleSheet.create({
+  actionButtonIcon: {
+    fontSize: 20,
+    height: 22,
+    color: 'white',
+  },
+});
+
+class QuickAdd extends PureComponent<Props, State> {
+  render() {
+    const { navigation } = this.props;
+    const { Item } = ActionButton;
+
+    return (
+      <ActionButton buttonColor="rgba(231,76,60,1)">
+        <Item buttonColor="#3498db" onPress={() => ({})}>
+          <IconComponent name="bone" style={styles.actionButtonIcon} />
+        </Item>
+        <Item
+          buttonColor="#1abc9c"
+          onPress={() => navigation.navigate('PoopAdd')}
+        >
+          <IconComponent name="emoticon-poop" style={styles.actionButtonIcon} />
+        </Item>
+      </ActionButton>
+    );
+  }
+}
+
+export default QuickAdd;
