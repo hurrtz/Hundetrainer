c06ccde328d39d258719bee6a836cf41bdd44c6f
Tobias Winkler <info@tobiaswinkler.berlin>
2019-05-16 23:05:45 +0200

    [food] added default screens for when no data available
    Signed-off-by: Tobias Winkler <info@tobiaswinkler.berlin>

--- src/stories/screens/Food/Options/Overview/index.tsx
+++ src/stories/screens/Food/Options/Overview/index.tsx
@@ -1,5 +1,10 @@
-import React, { ReactElement, FunctionComponent } from 'react';
-import { Screen, NavigationBar, Icon, Title } from '@shoutem/ui';
+import React, {
+  ReactElement,
+  FunctionComponent,
+  Fragment,
+  ReactNode,
+} from 'react';
+import { Screen, NavigationBar, Icon, Title, Text, View } from '@shoutem/ui';
 
 import { StandardView } from 'ui/Layout';
 
@@ -9,19 +14,65 @@ interface Props {
 
 const OptionsOverview: FunctionComponent<Props> = ({
   navigation,
-}: Props): ReactElement => (
-  <Screen>
-    <NavigationBar
-      leftComponent={
-        <Icon name="sidebar" onPress={(): void => navigation.toggleDrawer()} />
-      }
-      title="Übersicht"
-      styleName="inline"
-    />
-    <StandardView>
-      <Title>Übersicht über alle angelegten Nahrungsmittel</Title>
-    </StandardView>
-  </Screen>
-);
+}: Props): ReactElement => {
+  const createDefault = (): ReactElement => (
+    <Fragment>
+      <Title>Keine Daten vorhanden</Title>
+      <Text styleName="md-gutter-top">
+        Hier können Sie konkrete Nahrungsmittel hinterlegen, die Ihr Hund häufig
+        isst. Auf diese Weise können Sie sowohl konkreter tracken, was Ihr Hund
+        gerade zu sich genommen hat, als auch genauere Informationen zu
+        einzelnen Nahrungsmitteln festhalten, wie beispiesweise Inhaltsstoffe
+        oder Bezugsort.
+      </Text>
+    </Fragment>
+  );
+
+  const createFoodOptionsLists = (): ReactNode => {
+    const dates = Object.keys({});
+
+    if (dates.length === 0) {
+      return createDefault();
+    }
+
+    const datesSorted = [...dates].sort(
+      (dateA, dateB): number => {
+        if (dateA > dateB) {
+          return -1;
+        }
+
+        if (dateB > dateA) {
+          return 1;
+        }
+
+        return 0;
+      },
+    );
+
+    return datesSorted.map(
+      (date): ReactElement => (
+        <View key={date} styleName="md-gutter-top">
+          <Text>ein getracktes Nahrungsmittel</Text>
+        </View>
+      ),
+    );
+  };
+
+  return (
+    <Screen>
+      <NavigationBar
+        leftComponent={
+          <Icon
+            name="sidebar"
+            onPress={(): void => navigation.toggleDrawer()}
+          />
+        }
+        title="Übersicht"
+        styleName="inline"
+      />
+      <StandardView>{createFoodOptionsLists()}</StandardView>
+    </Screen>
+  );
+};
 
 export default OptionsOverview;
--- src/stories/screens/Food/Tracked/Overview/index.tsx
+++ src/stories/screens/Food/Tracked/Overview/index.tsx
@@ -1,5 +1,10 @@
-import React, { ReactElement, FunctionComponent } from 'react';
-import { Screen, NavigationBar, Icon, Title } from '@shoutem/ui';
+import React, {
+  ReactElement,
+  FunctionComponent,
+  Fragment,
+  ReactNode,
+} from 'react';
+import { Screen, NavigationBar, Icon, Title, Text, View } from '@shoutem/ui';
 
 import { StandardView } from 'ui/Layout';
 
@@ -9,19 +14,63 @@ interface Props {
 
 const TrackedOverview: FunctionComponent<Props> = ({
   navigation,
-}: Props): ReactElement => (
-  <Screen>
-    <NavigationBar
-      leftComponent={
-        <Icon name="sidebar" onPress={(): void => navigation.toggleDrawer()} />
-      }
-      title="Übersicht"
-      styleName="inline"
-    />
-    <StandardView>
-      <Title>Übersicht über alle getrackten Nahrungsmittel</Title>
-    </StandardView>
-  </Screen>
-);
+}: Props): ReactElement => {
+  const createDefault = (): ReactElement => (
+    <Fragment>
+      <Title>Keine Daten vorhanden</Title>
+      <Text styleName="md-gutter-top">
+        Hier können Sie tracken, was Ihr Hund isst und zu sich nimmt. Wenn Ihr
+        Hund oft die gleichen Nahrungsmittel isst, hinterlegen Sie diese bitte
+        im nächsten Tab, um schnell Zugriff darauf zu haben.
+      </Text>
+    </Fragment>
+  );
+
+  const createTrackedFoodsLists = (): ReactNode => {
+    const dates = Object.keys({});
+
+    if (dates.length === 0) {
+      return createDefault();
+    }
+
+    const datesSorted = [...dates].sort(
+      (dateA, dateB): number => {
+        if (dateA > dateB) {
+          return -1;
+        }
+
+        if (dateB > dateA) {
+          return 1;
+        }
+
+        return 0;
+      },
+    );
+
+    return datesSorted.map(
+      (date): ReactElement => (
+        <View key={date} styleName="md-gutter-top">
+          <Text>ein getracktes Nahrungsmittel</Text>
+        </View>
+      ),
+    );
+  };
+
+  return (
+    <Screen>
+      <NavigationBar
+        leftComponent={
+          <Icon
+            name="sidebar"
+            onPress={(): void => navigation.toggleDrawer()}
+          />
+        }
+        title="Übersicht"
+        styleName="inline"
+      />
+      <StandardView>{createTrackedFoodsLists()}</StandardView>
+    </Screen>
+  );
+};
 
 export default TrackedOverview;
