eaf0cd3d97991befeff144133e4f143a7314333f
Tobias Winkler <info@tobiaswinkler.berlin>
2019-05-17 22:33:10 +0200

    migrated App.tsx to functional component (forgot about that...)
    Signed-off-by: Tobias Winkler <info@tobiaswinkler.berlin>

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Component, Fragment, ReactElement } from 'react';
+import React, { FunctionComponent, Fragment, ReactElement } from 'react';
 import { StatusBar } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 
@@ -6,15 +6,11 @@ import DrawerNavigator from 'navigation/drawer';
 
 const Navigation = createAppContainer(DrawerNavigator);
 
-class AppRoot extends Component {
-  render(): ReactElement {
-    return (
-      <Fragment>
-        <StatusBar hidden />
-        <Navigation />
-      </Fragment>
-    );
-  }
-}
+const AppRoot: FunctionComponent<{}> = (): ReactElement => (
+  <Fragment>
+    <StatusBar hidden />
+    <Navigation />
+  </Fragment>
+);
 
 export default AppRoot;
